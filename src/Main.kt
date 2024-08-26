9e6ebc41668217ef52a5cdcf1632972970de72e9
Admin <reteamboss@gmail.com>
2024-08-26 20:14:49 +0300

    Задание 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,7 @@
 fun main() {
-    println("Hello World!")
+    var registration = Registration()
+    registration.setEmail("Email@mail.ru")
+    registration.setPassword("1234433")
+    println(registration.getEmail())
+    println(registration.getPassword())
 }
--- src/Registration.kt
+++ src/Registration.kt
@@ -1,2 +1,20 @@
 class Registration {
+
+    private var email: String = ""
+    private var password: String = ""
+
+    fun getEmail() = email
+    fun getPassword() = password
+
+    fun setPassword(password: String) {
+        if (password.length < 6){
+            println("Пароль меньше 6 символов")
+        } else {
+            this.password = password
+        }
+
+    }
+    fun setEmail(email: String){
+        this.email = email
+    }
 }
